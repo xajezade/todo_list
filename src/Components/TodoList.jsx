210e28fe7026057107c8cc4e2e07ab386702c9bc
xajezade <ali.xajezade@gmail.com>
2024-09-16 20:55:08 -0400

    Added functionality to add a todo header and display it

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,8 +2,16 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+  const [todos, setTodos] = useState([])
+  const [headingInput, setHeadingInput] = useState('')
+  const [listInputs, setListInputs] = useState({})
+  const handleAddTodo = () => {
+    if(headingInput.trim() !== ''){
+        setTodos([...todos,{ heading: headingInput, lists: []}]);
+        setHeadingInput('');
+    }
 
+  };
   return (
     <>
       <div className="todo-container">
@@ -13,13 +21,21 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}}
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
-        
+        {todos.map((todo, index) =>(
+            <div key={index} className='todo-card'>
+                <div className='heading_todo'>
+                    <h3>{todo.heading}</h3>{/* Display the heading here */}
+                    <button className='delete-button-heading'>Delete Heading</button>
+                </div>
+            </div>
+        ))}
       </div>
     </>
   );
